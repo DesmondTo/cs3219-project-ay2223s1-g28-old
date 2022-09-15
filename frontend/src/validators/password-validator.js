b146acf35d5452cf0603f568ca13341e9fca4c6d
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-15 10:20:06 +0800

    Initialize regex

--- frontend/src/validators/password-validator.js
+++ frontend/src/validators/password-validator.js
@@ -1,6 +1,6 @@
 import { isNotEmpty } from "./generic-validator";
 
-const STRONG_PASSWORD_REGEX = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$";
+const STRONG_PASSWORD_REGEX = new RegExp("^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$");
 
 const isStrongPassword = (value) => STRONG_PASSWORD_REGEX.test(value);
 
