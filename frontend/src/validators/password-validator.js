7a02b7c27b5ae483d98b2841dab3dbacf8962b78
Desmond To Cie Hong <desmond0429@Desmonds-MacBook-Pro-2.local>
2022-09-15 16:25:38 +0800

    Use simpler password validator for signin

--- frontend/src/validators/password-validator.js
+++ frontend/src/validators/password-validator.js
@@ -1,12 +1,8 @@
 import { isNotEmpty } from "./generic-validator";
 
-const STRONG_PASSWORD_REGEX = new RegExp("^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$");
-
-const isStrongPassword = (value) => STRONG_PASSWORD_REGEX.test(value);
-
 const isValidPassword = (value) => {
     // Can add more validate functions here in the future
-    return isNotEmpty(value) && isStrongPassword(value);
+    return isNotEmpty(value);
 }
 
 export default isValidPassword;
